56e7b79072c82f094ea74247e03837c6b1b28a6c
thang.buingoc <winsnyder@thangbugocsMini.lan>
2023-04-06 12:32:58 +0700

    init source folder pattern - clean architecture with vue2

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Product />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Product from "./module/product/view/product.vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {
+    Product,
+  },
+});
+</script>
